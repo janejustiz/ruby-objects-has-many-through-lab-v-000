eb44abf093edb320198891a6304b15594f952646
Jane Justiz <janejustiz@gmail.com>
2018-08-01 03:04:04 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -4,11 +4,18 @@ class Patient
   @@all = []
 
   def self.all
-    
+
   end
 
   def initialize(name)
     @name = name
     @@all << self
+  end
+
+  def new_appointment
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+  
+  def doctors
   end
 end
