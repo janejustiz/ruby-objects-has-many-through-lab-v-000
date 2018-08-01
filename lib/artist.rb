bd378c5ce5a16c9d7017c613c6b064bf68b3c510
Jane Justiz <janejustiz@gmail.com>
2018-08-01 02:46:42 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,13 +3,13 @@ class Artist
 
   @@all = []
 
+  def self.all
+    @@all
+  end
+
   def initialize(name)
     @name = name
     @@all << self
-  end
-
-  def self.all
-    @@all
   end
 
   def new_song(song, genre)
